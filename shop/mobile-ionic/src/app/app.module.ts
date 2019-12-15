9f52818dfd9352834f7a323adbfffe4ad2069f23
leejehwan <dlwpghks1191@gmail.com>
2019-12-15 20:38:45 +0900

    Merge

--- shop/mobile-ionic/src/app/app.module.ts
+++ shop/mobile-ionic/src/app/app.module.ts
@@ -53,12 +53,8 @@ import { HttpModule } from '@angular/http';
 import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
 import { ReactiveFormsModule } from '@angular/forms';
 import { VideoRoomPageModule } from './video-room/video-room.module';
-<<<<<<< HEAD
-//import { CallRoomPageModule } from './call-room/call-room.module';
-=======
 import { Toast } from '@ionic-native/toast/ngx';
 
->>>>>>> 7d1b361bfb9c525bed623bb6e82bb0c4fa6deb08
 @NgModule({
 	declarations: [AppComponent],
 	imports: [
